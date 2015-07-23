30c96d0b3401b093c55fd2808be8484364eaa845
mikey179 <github@frankkleine.de>
2015-07-23 14:02:21 +0200

    fix title

--- src/main/php/ApiHtml.php
+++ src/main/php/ApiHtml.php
@@ -166,7 +166,7 @@ EOD;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title>BK Feature Stats: $headline</title>
+    <title>Hello World API: $headline</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
   </head>
   <body>
