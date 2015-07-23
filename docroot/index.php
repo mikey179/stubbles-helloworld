a8d155729d1dba2153544f35fdb3262601fe4abc
Frank Kleine <github@frankkleine.de>
2015-07-23 14:49:30 +0200

    remove start time

--- docroot/index.php
+++ docroot/index.php
@@ -4,6 +4,5 @@
  */
 namespace stubbles\helloworld;
 use stubbles\lang\Rootpath;
-define('START_TIME', microtime(true));
 require __DIR__ . '/../vendor/autoload.php';
 HelloWorldApp::create(new Rootpath())->run()->send();
